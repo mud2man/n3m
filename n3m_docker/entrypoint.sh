530fdef0eea04055bedca6237e5a988f9c0ced98
m.hopkins <m.hopkins@tmcbonds.com>
2016-09-28 12:51:29 -0400

    fixing typos breaking app installation

--- n3m_docker/entrypoint.sh
+++ n3m_docker/entrypoint.sh
@@ -5,13 +5,17 @@ cd /home/n3m/git
 if [ ! -d "./n3m" ]; then
   source /home/n3m/.bashrc
   mkdir /home/n3m/.vimundo
+  sudo npm cache clean -f
+  sudo npm install -g n
+  sudo n stable
+  sudo npm install npm -g
   git clone https://github.com/team-n3m/n3m.git
   cd n3m
   virtualenv venv
   . venv/bin/activate 
   pip install -r requirements.txt
   sudo -H -u postgres bash -c "createuser -r -s -d --replication n3m"
-  creaetdb n3mdb
+  createdb n3mdb
   python manage.py create_db
   python manage.py n3mdb upgrade
   python manage.py n3mdb migrate 
